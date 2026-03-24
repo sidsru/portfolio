94332ea1d7ac95272bf07d29c6c98c6bea9e2d51
ParkChanYeong <sidsru21131210@gmail.com>
2026-03-24 23:55:31 +0900

    지피티

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production'
-const repoName = 'portfolio' // 본인 레포 이름으로 변경
-
 const nextConfig = {
   output: 'export',
-
   trailingSlash: true,
   images: {
     unoptimized: true,
